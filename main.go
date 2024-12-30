5ea00bf4178afac26f8294ff0c257a15bfacef4d
danilo <danilosindric@gmail.com>
2024-12-30 16:35:10 +0100

    Add: chi and godotenv packages

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)	
+	}
+
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
+	})
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	log.Println("Listening on port", listenAddr)
+	http.ListenAndServe(listenAddr, router)
 }
