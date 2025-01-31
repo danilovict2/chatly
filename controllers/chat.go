0b333578aeea71957bfcc4bfe1ba18fb29b94d8d
danilo <danilosindric@gmail.com>
2025-01-31 17:41:03 +0100

    Add: message controller

--- controllers/chat.go
+++ controllers/chat.go
@@ -4,10 +4,8 @@ import (
 	"net/http"
 
 	"github.com/danilovict2/go-real-time-chat/internal/database"
-	"github.com/danilovict2/go-real-time-chat/internal/pusher"
 	"github.com/danilovict2/go-real-time-chat/models"
 	"github.com/danilovict2/go-real-time-chat/views/chat"
-	"github.com/danilovict2/go-real-time-chat/views/components"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -43,58 +41,4 @@ func ChatShow(w http.ResponseWriter, r *http.Request) ControllerError {
 	}
 
 	return Render(w, r, chat.Chat(sender, receiver, messages))
-}
-
-func MessageStore(w http.ResponseWriter, r *http.Request) ControllerError {
-	messageImage, controllerErr := SaveFormFile(r, "message_image")
-	if controllerErr != (ControllerError{}) {
-		return controllerErr
-	}
-
-	receiverUsername := chi.URLParam(r, "receiverUsername")
-	db, err := database.NewConnection()
-	if err != nil {
-		return ControllerError{
-			err:  err,
-			code: http.StatusInternalServerError,
-		}
-	}
-
-	receiver := &models.User{}
-	if err := db.Where("username = ?", receiverUsername).First(receiver).Error; err != nil {
-		return ControllerError{
-			err:  err,
-			code: http.StatusInternalServerError,
-		}
-	}
-
-	sender, _ := r.Context().Value(userContextKey).(*models.User)
-
-	message := models.Message{
-		SenderID:   sender.ID,
-		ReceiverID: receiver.ID,
-		Text:       r.PostFormValue("message"),
-		Image:      messageImage,
-	}
-
-	if err := db.Create(&message).Error; err != nil {
-		return ControllerError{
-			err:  err,
-			code: http.StatusInternalServerError,
-		}
-	}
-
-	data := map[string]string{
-		"Text":         message.Text,
-		"Image":        message.Image,
-		"Sender":       sender.Username,
-		"SenderAvatar": components.Avatar(*sender),
-		"CreatedAt":    message.CreatedAt.Format("3:04 PM"),
-	}
-
-	pusherClient := pusher.NewClient()
-	pusherClient.Trigger("message", "to."+receiverUsername, data)
-
-	http.Redirect(w, r, "/chat/"+receiver.Username, http.StatusSeeOther)
-	return ControllerError{}
 }
